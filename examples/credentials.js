63f7108f62973f9eaa6708252a7cd19507355f52
VitorMarm <vitorccmf@gmail.com>
2025-10-13 17:00:18 -0400

    isso

--- examples/credentials.js
+++ examples/credentials.js
@@ -2,7 +2,8 @@ module.exports = {
 	// PRODUÇÃO = false
 	// HOMOLOGAÇÃO = true
 	sandbox: false,
-	client_id: 'seu_client_id',
-	client_secret: 'seu_client_secret',
-	certificate: 'caminho/ate/seu/certificado.p12',
+	client_id: 'Client_Id_b8835c974c4ab781cfb8b82d035300a01a68d7be',
+	client_secret: 'Client_Secret_f97ffcb9e9ebc40f5e16b609cd4af8a5eb50bc70',
+	certificate: 'F:/IA/Vibe Coding/AtmozPay/sdk-node-apis-efi/EFI BANK/producao-838769-AtmozPay - Prod.p12',
+	cert_base64: false,
 }
